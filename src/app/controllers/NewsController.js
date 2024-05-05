462a6b3c8042ab0a0f8c2f752597f17f44b0bddb
nken <nken3820@gmai.com>
2024-05-06 01:26:03 +0700

    view news

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,7 +1,32 @@
+const Post = require('../models/Post');
+const { mongooseToObject } = require('../../util/mongoose');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+
 class NewsController {
     // [Get] /news
-    index(req, res) {
-        res.render('news');
+    show(req, res, next) {
+        // Post.findOne({slug: req.params.slug})
+        // .then((posts) => {
+        //     res.render('news',{
+        //         posts: mongooseToObject(posts),
+        //     });
+        // })
+        // .catch(next);
+
+        Promise.all([
+            Post.find({}).then((posts) => mutipleMongooseToObject(posts)),
+            Post.findOne({ slug: req.params.slug }).then((post) =>
+                mongooseToObject(post),
+            ),
+        ])
+            .then(([posts, post]) => {
+                // Truyền dữ liệu vào hàm render
+                res.render('news', {
+                    posts: posts,
+                    post: post,
+                });
+            })
+            .catch(next);
     }
 }
 
--- src/routes/news.route.js
+++ src/routes/news.route.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 const newsController = require('../app/controllers/NewsController');
 
-router.get('/', newsController.index);
+router.get('/:slug', newsController.show);
 
 module.exports = router;
