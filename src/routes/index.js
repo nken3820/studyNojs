c8fd346f6238747bc17ef19cc683594a9ff3c8ee
nken <nken3820@gmai.com>
2024-05-05 13:37:14 +0700

    change version husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,8 @@ const newsRouter = require('./news.route');
 const siteRouter = require('./site.route');
 
 function route(app) {
-                app.use('/news', newsRouter);
+    
+    app.use('/news', newsRouter);
 
     app.use('/', siteRouter);
 }
