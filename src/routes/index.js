7df0caf26d88c079928f7f9a10f9baf3bbcc8bfc
nken <nken3820@gmai.com>
2024-05-05 13:42:53 +0700

    uninstall husky

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,7 @@ const newsRouter = require('./news.route');
 const siteRouter = require('./site.route');
 
 function route(app) {
-
-                app.use('/news', newsRouter);
+    app.use('/news', newsRouter);
 
     app.use('/', siteRouter);
 }
